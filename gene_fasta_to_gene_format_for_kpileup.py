2398d1e7b4ff17fc8b5ba8134def90f1be5d0496
Gavin Douglas <gavinmdouglas@gmail.com>
2021-08-30 15:42:54 -0400

    added option to limit output to specified genes

--- gene_fasta_to_gene_format_for_kpileup.py
+++ gene_fasta_to_gene_format_for_kpileup.py
@@ -16,6 +16,10 @@ def main():
     parser.add_argument("-f", "--fasta", metavar="FASTA", type=str,
                         help="Path to gene FASTA", required=True)
 
+    parser.add_argument("-g", "--gene_ids", metavar="GENE_IDS", type=str,
+                        help="Path to file with gene ids that should be retained.",
+                        required=False, default = None)
+
     parser.add_argument("-o", "--output", metavar="OUTPUT", type=str,
                         help="Path to directory where a single output gene-formatted file will be created for separate sequence in the input FASTA.",
                         required=True)
@@ -24,11 +28,21 @@ def main():
 
     seqs = read_fasta(args.fasta, cut_header = True)
 
+    if args.gene_ids is not None:
+        genes_to_keep = set()
+        with open(args.gene_ids, 'r') as GENE_IDS:
+            for line in GENE_IDS:
+                genes_to_keep.add(line.rstrip())
+
     os.mkdir(args.output)
 
     for seq_id in seqs.keys():
 
         outfile = args.output + "/" + seq_id + ".gene"
+
+        # Skip if not in set of genes to retain (if option specified).
+        if args.gene_ids is not None and seq_id not in genes_to_keep:
+            continue
 
         sequence = seqs[seq_id]
 
