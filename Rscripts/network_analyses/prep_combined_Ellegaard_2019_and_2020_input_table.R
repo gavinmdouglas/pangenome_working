50c5613265afd2092a2c8b60963f57d2ced9abce
Gavin Douglas <gavinmdouglas@gmail.com>
2021-11-09 16:08:27 -0500

    reran network commands

--- Rscripts/network_analyses/prep_combined_Ellegaard_2019_and_2020_input_table.R
+++ Rscripts/network_analyses/prep_combined_Ellegaard_2019_and_2020_input_table.R
@@ -2,45 +2,13 @@
 
 rm(list = ls(all.names = TRUE))
 
-pandora_output <- read.table("/data1/gdouglas/projects/honey_bee/combined_Ellegaard.2019.2020/pandora_running/pandora_multisample.matrix",
+pandora_output <- read.table("/data1/gdouglas/projects/honey_bee/combined_Ellegaard.2019.2020/pandora_running/pandora_out_illumina/pandora_multisample.matrix",
                              header = TRUE, sep = "\t", row.names = 1)
 rownames(pandora_output) <- gsub("\\.fa$", "", rownames(pandora_output))
 
 # Identify core genes
 
-core_genes <- list()
-
-Bifidobacterium_panaroo <- read.table("/data1/gdouglas/projects/honey_bee/ref_genome_pangenomes/panaroo_out_ref_only/Bifidobacterium/gene_presence_absence_roary.csv",
-                                      header = TRUE, sep = ",", comment.char = "", quote = "")
-rownames(Bifidobacterium_panaroo) <- paste("Bifidobacterium", Bifidobacterium_panaroo$Gene, sep = "_")
-core_genes[["Bifidobacterium"]] <- rownames(Bifidobacterium_panaroo)[which(Bifidobacterium_panaroo$No..isolates >= 14)]
-
-
-Firm4_panaroo <- read.table("/data1/gdouglas/projects/honey_bee/ref_genome_pangenomes/panaroo_out_ref_only/Firm4/gene_presence_absence_roary.csv.gz",
-                            header = TRUE, sep = ",", comment.char = "", quote = "")
-rownames(Firm4_panaroo) <- paste("Firm4", Firm4_panaroo$Gene, sep = "_")
-core_genes[["Firm4"]] <- rownames(Firm4_panaroo)[which(Firm4_panaroo$No..isolates >= 5)]
-
-
-Firm5_panaroo <- read.table("/data1/gdouglas/projects/honey_bee/ref_genome_pangenomes/panaroo_out_ref_only/Firm5/gene_presence_absence_roary.csv",
-                            header = TRUE, sep = ",", comment.char = "", quote = "")
-rownames(Firm5_panaroo) <- paste("Firm5", Firm5_panaroo$Gene, sep = "_")
-core_genes[["Firm5"]] <- rownames(Firm5_panaroo)[which(Firm5_panaroo$No..isolates >= 25)]
-
-
-Gilliamella_panaroo <- read.table("/data1/gdouglas/projects/honey_bee/ref_genome_pangenomes/panaroo_out_ref_only/Gilliamella/gene_presence_absence_roary.csv",
-                                  header = TRUE, sep = ",", comment.char = "", quote = "")
-rownames(Gilliamella_panaroo) <- paste("Gilliamella", Gilliamella_panaroo$Gene, sep = "_")
-core_genes[["Gilliamella"]] <- rownames(Gilliamella_panaroo)[which(Gilliamella_panaroo$No..isolates >= 98)]
-
-
-Snodgrassella_panaroo <- read.table("/data1/gdouglas/projects/honey_bee/ref_genome_pangenomes/panaroo_out_ref_only/Snodgrassella/gene_presence_absence_roary.csv",
-                                    header = TRUE, sep = ",", comment.char = "", quote = "")
-rownames(Snodgrassella_panaroo) <- paste("Snodgrassella", Snodgrassella_panaroo$Gene, sep = "_")
-core_genes[["Snodgrassella"]] <- rownames(Snodgrassella_panaroo)[which(Snodgrassella_panaroo$No..isolates >= 51)]
-
-all_cores_genes <- c(core_genes$Bifidobacterium, core_genes$Firm4, core_genes$Firm5, core_genes$Gilliamella, core_genes$Snodgrassella)
-
+panaroo_summary_and_core_genes <- readRDS("/data1/gdouglas/projects/honey_bee/ref_genome_pangenomes/panaroo_out_ref_only/combined_panaroo_and_core_genes.rds")
 
 # Table representing % of core genes called as present per phylotype per sample.
 core_gene_coverage <- data.frame(matrix(NA, ncol = 5, nrow = 74))
@@ -48,9 +16,12 @@ colnames(core_gene_coverage) <- c("Bifidobacterium", "Firm4", "Firm5", "Gilliame
 rownames(core_gene_coverage) <- colnames(pandora_output)
 
 for (phylotype in colnames(core_gene_coverage)) {
+  
+  phylotype_core_genes <- grep(phylotype, panaroo_summary_and_core_genes$core_genes, value = TRUE)
+  
   for (samplename in rownames(core_gene_coverage)) {
-    core_gene_coverage[samplename, phylotype] <- (length(which(pandora_output[core_genes[[phylotype]], samplename] == 1)) / length(core_genes[[phylotype]])) * 100
-  }   
+    core_gene_coverage[samplename, phylotype] <- length(which(pandora_output[phylotype_core_genes, samplename] == 1)) / length(phylotype_core_genes) * 100
+  }
 }
 
 max_coverage_per_sample <- apply(core_gene_coverage, 1, max, na.rm = TRUE)
@@ -63,13 +34,13 @@ colnames(pandora_output)[which(colSums(pandora_output) < 2000)]
 pandora_output_filt <- pandora_output[, -which(colSums(pandora_output) < 2000)]
 
 write.table(x = pandora_output_filt,
-            file = "/data1/gdouglas/projects/honey_bee/combined_Ellegaard.2019.2020/pandora_running/pandora_multisample.filt.matrix",
+            file = "/data1/gdouglas/projects/honey_bee/combined_Ellegaard.2019.2020/pandora_running/pandora_out_illumina/pandora_multisample.filt.matrix",
             sep = "\t", row.names = TRUE, col.names = NA, quote = FALSE)
 
 
-pandora_output_filt_noncore <- pandora_output_filt[-which(rownames(pandora_output_filt) %in% all_cores_genes), ]
+pandora_output_filt_noncore <- pandora_output_filt[-which(rownames(pandora_output_filt) %in% panaroo_summary_and_core_genes$core_genes), ]
 
 write.table(x = pandora_output_filt_noncore,
-            file = "/data1/gdouglas/projects/honey_bee/combined_Ellegaard.2019.2020/pandora_running/pandora_multisample.filt.noncore.matrix",
+            file = "/data1/gdouglas/projects/honey_bee/combined_Ellegaard.2019.2020/pandora_running/pandora_out_illumina/pandora_multisample.filt.noncore.matrix",
             sep = "\t", row.names = TRUE, col.names = NA, quote = FALSE)
 
