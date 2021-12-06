688a16b1f83e996d7712875d6ecc3e5b552507a6
Gavin Douglas <gavinmdouglas@gmail.com>
2021-12-06 10:45:06 -0500

    added dict of iupac bases

---
+++ functions/iupac.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+iupac_bases = dict()
+
+iupac_bases['M'] = ['A', 'C']
+iupac_bases['R'] = ['A', 'G']
+iupac_bases['W'] = ['A', 'T']
+
+iupac_bases['S'] = ['C', 'G']
+iupac_bases['Y'] = ['C', 'T']
+iupac_bases['K'] = ['G', 'T']
+
+
+iupac_bases['V'] = ['A', 'C', 'G']
+iupac_bases['H'] = ['A', 'C', 'T']
+iupac_bases['D'] = ['A', 'G', 'T']
+
+iupac_bases['B'] = ['C', 'G', 'T']
+
+iupac_bases['N'] = ['A', 'C', 'G', 'T']
+
