5329aad1ec772f5a329674a4c05b1cd8dc3d7820
Gavin Douglas <gavinmdouglas@gmail.com>
2022-01-31 14:56:38 -0500

    prep all starting files

--- R_scripts/strainfinder_running/prep_all_genefiles_and_samplefiles.R
+++ R_scripts/strainfinder_running/prep_all_genefiles_and_samplefiles.R
@@ -2,14 +2,14 @@
 
 rm(list = ls(all.names = TRUE))
 
-gene_matrix <- read.table("/data1/gdouglas/projects/honey_bee/combined_Ellegaard.2019.2020/pandora_running/pandora_out_illumina/pandora_multisample.filt.matrix",
+gene_matrix <- read.table("/data1/gdouglas/projects/honey_bee/combined_Ellegaard.2019.2020/pandora_running/pandora_out_illumina_all_present/pandora_multisample.matrix",
                           header = TRUE, sep = "\t", row.names = 1)
 
-setwd("/data1/gdouglas/projects/honey_bee/combined_Ellegaard.2019.2020/StrainFinder_running/all_individual_genes/starting_files")
+setwd("/data1/gdouglas/projects/honey_bee/combined_Ellegaard.2019.2020/StrainFinder_running/all_gene_input/starting_files")
 
 for (gene in rownames(gene_matrix)) {
 
-  genefile <- paste(gene, "_genefile.txt", sep = "") 
+  genefile <- paste(gene, "_genes.txt", sep = "") 
   write.table(gene, file = genefile, quote = FALSE, col.names = FALSE, row.names = FALSE)
   
   samplefile <- paste(gene, "_samples.txt", sep = "")
